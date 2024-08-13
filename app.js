0c5314c4a248bf0b9449baca5beb8de3f94a8c3f
Abdul Aziz <connect2abdulaziz@gmail.com>
2024-08-14 00:38:51 +0500

    sync

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.listen(PORT, async () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
   try {
     await sequelize.authenticate();
+    await sequelize.sync();
     console.log("Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
