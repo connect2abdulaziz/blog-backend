4c80e1235df40a3dc53bf32f2467a7027ae7ebf4
Abdul Aziz <connect2abdulaziz@gmail.com>
2024-08-13 17:18:36 +0500

    "."

--- db/models/index.js
+++ db/models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const {dbConfig: config} = require(__dirname + '../../config');
+const {dbConfig: config} = require('../../config');
 const db = {};
 const pg = require('pg');
 
