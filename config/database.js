8a643527689c89a74a73c0861fec219acfb32295
Abdul Aziz <connect2abdulaziz@gmail.com>
2024-08-14 00:16:09 +0500

    main

--- config/database.js
+++ config/database.js
@@ -1,8 +1,22 @@
 const {Sequelize} = require('sequelize');
-
-const env = process.env.NODE_ENV || 'development';
 const config = require('./config');
-const sequelize = new Sequelize(config[env]);
-module.exports = sequelize
+const pg = require('pg');
 
 
+console.log(config)
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    dialect: config.DIALECT,
+    dialectModule: pg,
+    dialectOptions: config.dialectOptions,
+  });
+} else {
+  sequelize = new Sequelize(config.POSTGRES_URL, {
+    dialect: config.DIALECT,
+    dialectModule: pg,
+    dialectOptions: config.dialectOptions,
+  });
+}
+
+module.exports = sequelize;
