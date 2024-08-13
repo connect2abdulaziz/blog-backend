28b8fe04d12e8a600c4059c41042e9c5e362976e
Abdul Aziz <connect2abdulaziz@gmail.com>
2024-08-14 00:24:37 +0500

    config

--- config/database.js
+++ config/database.js
@@ -1,9 +1,7 @@
 const {Sequelize} = require('sequelize');
-const config = require('./config');
+const {dbConfig:config} = require('./index');
 const pg = require('pg');
 
-
-console.log(config)
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], {
