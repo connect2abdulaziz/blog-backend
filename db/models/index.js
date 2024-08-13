609482af526cd7b31864581fe775976d4e362953
Abdul Aziz <connect2abdulaziz@gmail.com>
2024-08-13 16:09:44 +0500

    "Deployment"

--- db/models/index.js
+++ db/models/index.js
@@ -8,6 +8,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../../config/config.js')[env];
 const db = {};
+const pg = require('pg');
 
 let sequelize;
 if (config.use_env_variable) {
