4cd007d2d1f3cd04e616d83e38935a3ee35240b2
Abdul Aziz <connect2abdulaziz@gmail.com>
2024-08-16 15:25:24 +0500

    swagger

--- utils/pagination.js
+++ utils/pagination.js
@@ -1,16 +1,21 @@
-// utils/pagination.js
-
 /**
  * Generic pagination function to handle pagination for queries.
  *
  * @param {number} page - Current page number.
  * @param {number} limit - Number of items per page.
  * @param {object} queryOptions - Query options to be passed to the Sequelize query.
+ * @param {Model} Model - The Sequelize model to perform the query on.
  * @returns {object} - An object containing the paginated data and metadata.
  */
+const paginate = async (page = 1, limit = 10, queryOptions = {}, Model) => {
+  // Ensure page and limit are numbers and page is greater than 0
+  page = parseInt(page, 10) > 0 ? parseInt(page, 10) : 1;
+  limit = parseInt(limit, 10) > 0 ? parseInt(limit, 10) : 10;
 
-const paginate = async (page, limit, queryOptions, Model) => {
+  // Calculate the offset
   const offset = (page - 1) * limit;
+
+  // Perform the query with pagination
   const { count, rows } = await Model.findAndCountAll({
     ...queryOptions,
     limit,
@@ -25,6 +30,4 @@ const paginate = async (page, limit, queryOptions, Model) => {
   };
 };
 
-
 export default paginate;
-
