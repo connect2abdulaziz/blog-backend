05e70d53a33b114bc08d7be0859deaa363efa189
Abdul Aziz <connect2abdulaziz@gmail.com>
2024-08-20 14:48:25 +0500

    multer

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,15 +1,24 @@
 import path from 'path';
 import multer from 'multer';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 // Resolve __dirname equivalent
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Configure storage
+// Directory for uploads
+const uploadDir = path.join(__dirname, '../uploads');
+
+// Ensure the uploads directory exists
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+// Configure multer storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '../uploads')); 
+    cb(null, uploadDir); // Use the uploadDir variable
   },
   filename: (req, file, cb) => {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
