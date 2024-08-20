fa9d0dc2e5c128f9faf1380c1fbe3845afdb356b
Abdul Aziz <connect2abdulaziz@gmail.com>
2024-08-20 16:19:06 +0500

    fs

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,6 +1,7 @@
 import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 // Get the current directory in ES module environments
 const __filename = fileURLToPath(import.meta.url);
