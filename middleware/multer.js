3c9d7b2bf1342fa04cdf06f6dd96a52cb24a7ff6
Abdul Aziz <connect2abdulaziz@gmail.com>
2024-08-20 14:45:19 +0500

    multer

--- middleware/multer.js
+++ middleware/multer.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 // Configure storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, 'uploads')); // Adjust path as needed
+    cb(null, path.join(__dirname, '../uploads')); 
   },
   filename: (req, file, cb) => {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
