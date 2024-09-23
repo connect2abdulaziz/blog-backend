f004eb2b23c154710ecba0d8adcbf65a9e41d0c7
Abdul Aziz <connect2abdulaziz@gmail.com>
2024-09-23 12:26:27 +0500

    committed

--- middleware/corsHandler.ts
+++ middleware/corsHandler.ts
@@ -1,6 +1,7 @@
 // middleware/corsHandler.js
 import cors from 'cors';
 
+// Apply CORS middleware to all routes
 const corsHandler = cors({
   origin: '*', 
   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
